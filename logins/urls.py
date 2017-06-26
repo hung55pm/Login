235b0cdd8d886dd13f50fe329fdcde777f2147ca
VINNET\hungdn <hungdn@vinnet.vn>
2017-06-26 17:57:28 +0700

    sdsdsds

--- logins/urls.py
+++ logins/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login, name='index'),
+    url(r'^$', views.login, name='login'),
     url(r'^login$', views.login, name='login'),
     url(r'^register$', views.register, name='register'),
     url(r'^index$', views.index, name='index'),
